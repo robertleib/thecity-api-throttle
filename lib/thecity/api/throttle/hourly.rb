60aa3557d2e579e22f0d7c3e6e765c56640df7f8
Robbie Leib <robert.leib@gmail.com>
2012-01-10 10:52:44 -0800

    adding custom headers to notify users how many requests they have left

--- lib/thecity/api/throttle/hourly.rb
+++ lib/thecity/api/throttle/hourly.rb
@@ -4,7 +4,35 @@ module TheCity
   
       require 'rack/throttle'
 
-      class ApiDefender < Rack::Throttle::Hourly
+      class Hourly < Rack::Throttle::Hourly
+        
+        def initialize(app, options = {})
+          super
+        end
+        
+        def allowed?(request)
+          need_throttling?(request) ? cache_incr(request) <= max_per_window : true
+        end
+        
+        def call(env)
+          status, headers, body = super
+          request = Rack::Request.new(env)
+
+          if need_throttling?(request)
+            headers['X-RateLimit-Limit'] = max_per_window.to_s
+            headers['X-RateLimit-Remaining'] = ([0, max_per_window - (cache_get(cache_key(request)).to_i rescue 1)].max).to_s
+          end
+          [status, headers, body]
+        end
+        
+        def need_throttling?(request)
+          return false if request.env["REQUEST_PATH"] =~ /^\/$|^\/accounts|^\/keys/i
+          return true
+        end
+        
+        def client_identifier(request)
+          request.env['X_CITY_USER_ID'] || request.env['HTTP_X_CITY_USER_ID'] || request.ip.to_s
+        end
       end
   
     end
--- lib/thecity/api/throttle/version.rb
+++ lib/thecity/api/throttle/version.rb
@@ -1,7 +1,7 @@
 module TheCity
   module Api
     module Throttle 
-      VERSION = "0.0.3"
+      VERSION = "0.0.4"
     end
   end
 end
